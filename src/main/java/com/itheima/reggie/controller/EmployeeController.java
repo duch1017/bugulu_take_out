9f96e3b9ceafe4e3007fb50d3f2a9b91f185cc0c
duch <1470651260@qq.com>
2024-05-07 21:43:58 +0800

    员工状态启用禁用及基于jackson将Java对象转为json

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -128,4 +128,16 @@ public class EmployeeController {
 
         return Result.success(pageInfo);
     }
+
+    @PutMapping
+    public Result<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+
+        employeeService.updateById(employee);
+        return Result.success("员工信息修改成功");
+    }
 }
