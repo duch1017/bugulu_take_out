e01d4de92d439f25d919b9702c0c9594984be6bc
duch <1470651260@qq.com>
2024-05-07 16:50:33 +0800

    登录接口

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.itheima.reggie.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -18,6 +17,6 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         // 静态资源映射
         log.info("开始进行静态资源映射");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/static/backend/");
-        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+//        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 }
---
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.Result;
+import com.itheima.reggie.entity.Employee;
+import com.itheima.reggie.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    /*
+    @Autowired
+    @Qualifier("employeeServiceImpl")
+    */
+    @Resource(name = "employeeServiceImpl")
+    private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     *
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public Result<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        //md5加密password
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //数据库查询username
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //判断是否查到数据
+        if (emp == null) {
+            return Result.error("无此账号");
+        }
+
+        //比对密码
+        if (!emp.getPassword().equals(password)) {
+            return Result.error("密码错误");
+        }
+
+        //查看员工状态
+        if (emp.getStatus() == 0) {
+            return Result.error("账号禁用");
+        }
+
+        //登录成功，将id放入session
+        request.getSession().setAttribute("employee", emp.getId());
+        return Result.success(emp);
+    }
+}
